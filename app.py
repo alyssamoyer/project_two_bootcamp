a5651a370c0184467b919ef8497cf442981f2148
Alyssa <allym.moyer21@gmail.com>
2020-03-26 19:35:03 -0700

    Adding the python app for mongo and flask

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+# Import our pymongo library, which lets us connect our Flask app to our Mongo database.
+import pymongo
+
+# Create an instance of our Flask app.
+app = Flask(__name__)
+
+# Create connection variable
+conn = 'mongodb://localhost:27017'
+
+# Pass connection to the pymongo instance.
+client = pymongo.MongoClient(conn)
+
+# Create new database
+db = client.aus_fires
